e4b929936f4f10fcb800a11066141f23ee8e8a0c
Emine Noor <114260503+eminenoor@users.noreply.github.com>
2023-05-29 22:42:47 +0300

    swing updated to vscode code

--- AGS/src/main/java/swing/PanelGradiente.java
+++ AGS/src/main/java/swing/PanelGradiente.java
@@ -1,14 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package swing;
-
-/**
- *
- * @author emine
- */
-
 import java.awt.Color;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
